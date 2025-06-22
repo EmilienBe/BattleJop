4a8b2d9448321bcb77fcf666146b7ccaf891511f
Emilien BEZIE <emilien.eb@gmail.com>
2025-06-22 22:36:04 +0200

    Add TournamentService

---
+++ BattleJop.Api.Application/Services/Tournament/ITournamentService.cs
@@ -0,0 +1,10 @@
+﻿namespace BattleJop.Api.Application.Services.Tournament;
+
+public interface ITournamentService
+{
+    Task<Domain.TournamentAggregate.Tournament> AddAsync(string name, int numberOfRounds, CancellationToken cancellationToken);
+    Task<Domain.TournamentAggregate.Tournament> UpdateAsync(string name, CancellationToken cancellationToken);
+    Task<ICollection<Domain.TournamentAggregate.Tournament>> GetAllAsync(CancellationToken cancellationToken);
+    Task<Domain.TournamentAggregate.Tournament> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+    Task DeleteByIdAsync(Guid id, CancellationToken cancellationToken);
+}
