1a1be912c23452ed8e86173d1b2ef4e3cd7e6b73
AntoineLebDev <leblanc.antoine@live.fr>
2025-07-12 18:35:02 +0200

    add tournament settings page

--- Web/BattleJop.Web/Components/Pages/Home.razor.cs
+++ Web/BattleJop.Web/Components/Pages/Home.razor.cs
@@ -1,17 +1,10 @@
-﻿using BlazorBootstrap;
-using Microsoft.AspNetCore.Components;
+﻿using Microsoft.AspNetCore.Components;
 
 namespace BattleJop.Web.Components.Pages
 {
     public partial class Home(NavigationManager navigation)
     {
-        private Modal _modal;
-        private async Task CreateTournamentAsync() => await _modal?.ShowAsync();
-
         private void GoToRound() => navigation.NavigateTo("/round");
-        private void HandleModalReady(Modal modal)
-        {
-            _modal = modal;
-        }
+        private void GoToCreation() => navigation.NavigateTo("/settings/tournament");
     }
 }
---
+++ Web/BattleJop.Web/Components/Pages/Settings/TournamentSettings.razor.cs
@@ -0,0 +1,43 @@
+﻿using BattleJop.Web.Dto;
+using BlazorBootstrap;
+using Microsoft.AspNetCore.Components;
+
+namespace BattleJop.Web.Components.Pages.Settings
+{
+    public partial class TournamentSettings(NavigationManager navigation)
+    {
+        int RoundNumber { get; set; }
+        private List<TeamDto> Teams { get; set; } = [];
+
+        private Modal _modal = new();
+        protected override void OnInitialized()
+        {
+
+            if (Teams.Count == 0)
+            {
+                Teams.Add(new() { Name = "Les foufous", Players = new Tuple<string, string>("Maud", "Emilien") });
+            }
+        }
+        private void HandleModalReady(Modal modal)
+        {
+            _modal = modal;
+        }
+
+        private async Task OnShowModalClick() => await _modal?.ShowAsync();
+
+        private void OnAddTeam(TeamDto team)
+        {
+            Teams.Add(team);
+        }
+
+        private void OnDeleteTeam(TeamDto team)
+        {
+            Teams.Remove(team);
+        }
+
+        private void CreateTournament()
+        {
+            navigation.NavigateTo("/round");
+        }
+    }
+}
